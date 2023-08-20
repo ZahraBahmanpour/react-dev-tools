128537f34d7efd42a598be083a3813fe75cf7593
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-08-20 12:33:09 +0330

    add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
+function ListItem({ text }) {
+  return <li>{text}</li>;
+}
+function List({ numOfItems }) {
+  const items = [];
+  for (let i = 0; i <= numOfItems; i++) {
+    items.push(<ListItem text={i} />);
+  }
+  return <ul>{items}</ul>;
+}
+
 function App() {
-  return <div>React Dev Tools</div>;
+  const [value, setValue] = useState("");
+  return (
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <List numOfItems={200} />
+    </>
+  );
 }
 
 export default App;
